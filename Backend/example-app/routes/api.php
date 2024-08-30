f6f97cc99edc42b4fd8a389ee96c8f336e138e77
tranchinguyen1307 <145844563+tranchinguyen1307@users.noreply.github.com>
2024-08-30 21:35:31 +0700

    laravel

--- Backend/example-app/routes/api.php
+++ Backend/example-app/routes/api.php
@@ -10,6 +10,7 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+
 use App\Http\Controllers\Admin\BrandController;
 
 Route::prefix('admin')->group(function () {
