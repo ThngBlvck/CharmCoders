857ed2adb572785b5f074832581a55ab1e963647
toankhung <lyminhtoan2004@gmail.com>
2024-09-17 10:22:50 +0700

    crud blog category

---
+++ Backend/example-app/app/Http/Requests/Admin/StoreBlogCategoryRequest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Requests\Admin;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Validation\Rule;
+
+class StoreBlogCategoryRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        $rules = [
+            'name' => 'required|string|max:255|unique:blog_categories,name',
+        ];
+
+        if ($this->isMethod('put') || $this->isMethod('patch')) {
+            $id = $this->route('BlogCategory');
+
+            $rules['name'] = [
+                'required',
+                'string',
+                'max:255',
+                Rule::unique('blog_categories')->ignore($id),
+            ];
+        }
+        return $rules;
+    }
+
+    public function messages()
+    {
+        return [
+            'name.required' => 'Tên danh mục là bắt buộc.',
+            'name.unique' => 'Tên danh mục đã tồn tại.',
+            'name.max' => 'Tên danh mục không được vượt quá 255 ký tự.',
+        ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'success' => false,
+            'message' => 'Dữ liệu không hợp lệ.',
+            'errors' => $validator->errors()
+        ], 422));
+    }
+}
--- Backend/example-app/routes/api.php
+++ Backend/example-app/routes/api.php
@@ -3,8 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\BlogCategoryController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 Route::get('/categories', [CategoryController::class, 'index']);
+
+Route::apiResource('BlogCategory', BlogCategoryController::class);
