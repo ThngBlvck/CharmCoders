adf1890250a1d8e0d7cec4ecfc3a78deed066f35
tranchinguyen1307 <145844563+tranchinguyen1307@users.noreply.github.com>
2024-09-17 10:33:04 +0700

    Crud category

---
+++ Backend/example-app/app/Http/Requests/Admin/StoreCategoryRequest.php
@@ -0,0 +1,37 @@
+<?php
+namespace App\Http\Requests\Admin;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+class StoreCategoryRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|max:255',
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'name.required' => 'Tên danh mục là bắt buộc.',
+            'name.string' => 'Tên danh mục phải là một chuỗi ký tự hợp lệ.',
+            'name.max' => 'Tên danh mục không được vượt quá 255 ký tự.',
+        ];
+    }
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'success' => false,
+            'message' => 'Dữ liệu không hợp lệ.',
+            'errors' => $validator->errors()
+        ], 422));
+    }
+}
