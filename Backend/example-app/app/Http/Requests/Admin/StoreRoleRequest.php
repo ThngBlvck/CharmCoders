f731cf65aa611bc650d6f7c7988b811c9e8b0173
toankhung <lyminhtoan2004@gmail.com>
2024-09-19 16:05:16 +0700

    crud role

---
+++ Backend/example-app/app/Http/Requests/Admin/StoreRoleRequest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Requests\Admin;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+
+class StoreRoleRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        $rules = [
+            'name' => 'required|string|max:255|unique:roles,name',
+        ];
+
+        if ($this->isMethod('put') || $this->isMethod('patch')) {
+            $id = $this->route('role');
+            $rules['name'] = [
+                'required',
+                'string',
+                'max:255',
+                Rule::unique('roles')->ignore($id),
+            ];
+        }
+        return $rules;
+    }
+
+    public function messages()
+    {
+        return [
+            'name.required' => 'Vui lòng nhập vai trò.',
+            'name.unique' => 'Vai trò đã tồn tại.',
+            'name.max' => 'Không được vượt quá 255 ký tự.',
+        ];
+    }
+
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'success' => false,
+            'message' => 'Dữ liệu không hợp lệ.',
+            'errors' => $validator->errors()
+        ], 422));
+    }
+}
