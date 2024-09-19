26d45e2c0dafe521cd8fb32389197dec4ba6600d
tranchinguyen1307 <145844563+tranchinguyen1307@users.noreply.github.com>
2024-09-19 13:26:05 +0700

    Hoàn thiện Crud Products

---
+++ Backend/example-app/app/Http/Controllers/Admin/ProductController.php
@@ -0,0 +1,76 @@
+<?php
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use App\Http\Requests\Admin\StoreProductRequest;
+use Illuminate\Support\Facades\Storage;
+
+class ProductController extends Controller
+{
+    // Hiển thị danh sách sản phẩm
+    public function index()
+    {
+        $products = Product::all();
+        return response()->json($products);
+    }
+
+    // Tạo sản phẩm mới
+    public function store(StoreProductRequest $request)
+    {
+        $validatedData = $request->validated();
+
+
+        if ($request->hasFile('image')) {
+            $image = $request->file('image');
+            $imageName = time() . '_' . $image->getClientOriginalName();
+            $image->storeAs('public/images/products', $imageName);
+            $validatedData['image'] = 'Backend/storage/images/products/' . $imageName;
+        }
+
+        $product = Product::create($validatedData);
+        return response()->json($product, 201);
+    }
+
+    // Hiển thị chi tiết sản phẩm
+    public function show($id)
+    {
+        $product = Product::findOrFail($id);
+        return response()->json($product);
+    }
+
+    // Cập nhật sản phẩm
+    public function update(StoreProductRequest $request, $id)
+    {
+        $product = Product::findOrFail($id);
+
+        $validatedData = $request->validated();
+
+        if ($request->hasFile('image')) {
+            $image = $request->file('image');
+            $imageName = time() . '_' . $image->getClientOriginalName();
+            $image->storeAs('public/images/products', $imageName);
+            $validatedData['image'] = 'Backend/storage/images/products/' . $imageName;
+        }
+
+        $product->update($validatedData);
+        return response()->json($product, 200);
+    }
+
+    // Xóa sản phẩm
+    public function destroy($id)
+    {
+        $product = Product::findOrFail($id);
+
+        if ($product->image) {
+            Storage::disk('public')->delete($product->image);
+        }
+
+        $product->delete();
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Sản phẩm đã được xóa thành công.',
+        ], 200);
+    }
+}
---
+++ Backend/example-app/app/Models/Product.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'unit_price',
+        'sale_price',
+        'quantity',
+        'image',
+        'content',
+        'views',
+        'status',
+        'brand_id',
+        'category_id',
+    ];
+
+    public function brand()
+    {
+        return $this->belongsTo(Brand::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function getPrice()
+    {
+        return $this->sale_price ?? $this->unit_price;
+    }
+}
