dff7e93eb3c4825224f59d1e80b92051c0f70809
tranchinguyen1307 <145844563+tranchinguyen1307@users.noreply.github.com>
2024-09-19 12:58:23 +0700

    Hoàn thiện Crud danh mục sản phẩm

--- Backend/example-app/app/Http/Requests/Admin/StoreCategoryRequest.php
+++ Backend/example-app/app/Http/Requests/Admin/StoreCategoryRequest.php
@@ -1,21 +1,36 @@
 <?php
+
 namespace App\Http\Requests\Admin;
 
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Validation\Rule;
+
+
 class StoreCategoryRequest extends FormRequest
 {
     public function authorize(): bool
     {
         return true;
     }
-
     public function rules(): array
     {
-        return [
-            'name' => 'required|string|max:255',
+        $rules = [
+            'name' => 'required|string|max:255|unique:categories,name',
         ];
+
+        if ($this->isMethod('put') || $this->isMethod('patch')) {
+            $id = $this->route('category');
+
+            $rules['name'] = [
+                'required',
+                'string',
+                'max:255',
+                Rule::unique('categories')->ignore($id),
+            ];
+        }
+        return $rules;
     }
 
     public function messages(): array
@@ -24,10 +39,14 @@ class StoreCategoryRequest extends FormRequest
             'name.required' => 'Tên danh mục là bắt buộc.',
             'name.string' => 'Tên danh mục phải là một chuỗi ký tự hợp lệ.',
             'name.max' => 'Tên danh mục không được vượt quá 255 ký tự.',
+            'name.unique' => 'Tên danh mục đã tồn tại.',
         ];
     }
+
     public function failedValidation(Validator $validator)
     {
+
+
         throw new HttpResponseException(response()->json([
             'success' => false,
             'message' => 'Dữ liệu không hợp lệ.',
