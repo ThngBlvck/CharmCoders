d4d3fae77e5f6d74931dfcbfbfddf867b3da8574
toankhung <lyminhtoan2004@gmail.com>
2024-09-19 15:58:12 +0700

    crud role

--- Backend/example-app/routes/api.php
+++ Backend/example-app/routes/api.php
@@ -5,16 +5,16 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\BlogCategoryController;
 use App\Http\Controllers\Admin\BlogController;
+use App\Http\Controllers\Admin\RoleController;
 
 
 
 use App\Http\Controllers\Admin\BrandController;
 
 Route::prefix('admin')->group(function () {
-
     Route::apiResource('brands', BrandController::class);
     Route::apiResource('blog', BlogController::class);
     Route::apiResource('blogCategory', BlogCategoryController::class);
     Route::apiResource('productCategory', CategoryController::class);
-
+    Route::apiResource('role', RoleController::class);
 });
