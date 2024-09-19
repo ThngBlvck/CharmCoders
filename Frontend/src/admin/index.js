25e89700689679bccefd08ca1b90865a6a74ca0c
ThngBlvck <thonghhpc05659@fpt.edu.vn>
2024-09-19 17:37:01 +0700

    CRUD Danh mục bài viết

--- Frontend/src/admin/index.js
+++ Frontend/src/admin/index.js
@@ -12,25 +12,17 @@ import Dashboard from "./Pages/Dashboard";
 import Product from "./Pages/Product/List";
 import ProductCategory from "./Pages/ProductCategory/List";
 import Blog from "./Pages/Blog";
-import Brand from "./Pages/Brand/List";
-import Comment from "./Pages/Comment/List";
-import Role from "./Pages/Role/List";
+import Brand from "./Pages/Brand";
+import Comment from "./Pages/Comment";
 import Order from "./Pages/Order";
 import BlogCaterogy from "./Pages/BlogCategory/List";
 import AddProductCategory from "./Pages/ProductCategory/Add";
-import EditProductCaterogy from "./Pages/ProductCategory/Edit";
 
 import AddBlogCategory from "./Pages/BlogCategory/Add";
 import EditBlogCategory from "./Pages/BlogCategory/Edit";
 
 
-import EditBrand from "./Pages/Brand/Edit";
-import AddBrand from "./Pages/Brand/Add";
-
-import AddRole from "./Pages/Role/Add";
-import EditRole from "./Pages/Role/Edit";
-
-import EditProductCaterogy from "./Pages/ProductCategory/Edit";
+// import Maps from "views/admin/Maps.js";
 // import Settings from "views/admin/Settings.js";
 // import Tables from "views/admin/Tables.js";
 
@@ -48,7 +40,7 @@ export default function Admin() {
 
                         <Route path="category_product" element={<ProductCategory />} />
                         <Route path="category_product/add" element={<AddProductCategory />} />
-                        <Route path="category_product/edit/:id" element={<EditProductCaterogy />} />
+                        {/*<Route path="category_product/edit" element={<EditProductCaterogy />} />*/}
 
                         <Route path="product" element={<Product />} />
                         {/*<Route path="user" element={<User />} />*/}
@@ -56,14 +48,9 @@ export default function Admin() {
                         <Route path="category_blog/add" element={<AddBlogCategory />} />
                         <Route path="category_blog/edit/:id" element={<EditBlogCategory />} />
                         <Route path="blog" element={<Blog />}/>
-                        <Route path="brand" element={<Brand/>}/>
-                        <Route path="brand/add" element={<AddBrand/>}/>
-                        <Route path="brand/edit/:id" element={<EditBrand/>}/>
+                        <Route path="brand" element={<Brand />}/>
                         <Route path="comment" element={<Comment />}/>
                         <Route path="order" element={<Order />}/>
-                        <Route path="role" element={<Role />}/>
-                        <Route path="role/add" element={<AddRole />}/>
-                        <Route path="role/edit/:id" element={<EditRole />}/>
                         <Route path="/" element={<Navigate to="/admin/dashboard" />} />
                     </Routes>
                     <Footer />
