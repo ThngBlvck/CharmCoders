dd95a90e60950486f2bd7835e705f7d1751b9bbc
ThngBlvck <thonghhpc05659@fpt.edu.vn>
2024-09-21 01:08:15 +0700

    Brand Thông Báo

--- Frontend/src/admin/index.js
+++ Frontend/src/admin/index.js
@@ -12,11 +12,14 @@ import Dashboard from "./Pages/Dashboard";
 import Product from "./Pages/Product/List";
 import ProductCategory from "./Pages/ProductCategory/List";
 import Blog from "./Pages/Blog";
-import Brand from "./Pages/Brand";
+import Brand from "./Pages/Brand/List";
 import Comment from "./Pages/Comment";
 import Order from "./Pages/Order";
 import BlogCaterogy from "./Pages/BlogCategory/List";
 import AddProductCategory from "./Pages/ProductCategory/Add";
+
+import AddBrand from "./Pages/Brand/Add";
+import EditBrand from "./Pages/Brand/Edit";
 
 import AddBlogCategory from "./Pages/BlogCategory/Add";
 import EditBlogCategory from "./Pages/BlogCategory/Edit";
@@ -49,6 +52,8 @@ export default function Admin() {
                         <Route path="category_blog/edit/:id" element={<EditBlogCategory />} />
                         <Route path="blog" element={<Blog />}/>
                         <Route path="brand" element={<Brand />}/>
+                        <Route path="brand/add" element={<AddBrand />}/>
+                        <Route path="brand/edit/:id" element={<EditBrand />}/>
                         <Route path="comment" element={<Comment />}/>
                         <Route path="order" element={<Order />}/>
                         <Route path="/" element={<Navigate to="/admin/dashboard" />} />
