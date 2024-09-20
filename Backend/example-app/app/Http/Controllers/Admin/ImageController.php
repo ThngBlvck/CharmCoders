68abb928eaca5af55cddcef2f5adb4819d4d63e6
tranchinguyen1307 <145844563+tranchinguyen1307@users.noreply.github.com>
2024-09-20 10:21:13 +0700

    Hoàn thiện CRUD Image(Album ảnh)

--- Backend/example-app/app/Http/Controllers/Admin/ImageController.php
+++ Backend/example-app/app/Http/Controllers/Admin/ImageController.php
@@ -50,7 +50,6 @@ class ImageController extends Controller
         ], 200);
     }
 
-
     public function show($id)
     {
         $image = Image::findOrFail($id);
@@ -77,7 +76,7 @@ class ImageController extends Controller
 
                 // Cập nhật thông tin ảnh trong cơ sở dữ liệu
                 $image->update([
-                    'name' => $path,  // Đường dẫn ảnh mới 
+                    'name' => $path,  // Đường dẫn ảnh mới
                 ]);
             }
         }
