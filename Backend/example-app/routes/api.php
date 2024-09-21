a43140331b437af9b62a99c5ebae8b5a35c3d6f2
toankhung <lyminhtoan2004@gmail.com>
2024-09-21 10:55:34 +0700

    crud comment

--- Backend/example-app/routes/api.php
+++ Backend/example-app/routes/api.php
@@ -5,61 +5,15 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\BlogCategoryController;
 use App\Http\Controllers\Admin\BlogController;
-
 use App\Http\Controllers\Admin\RoleController;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 use App\Http\Controllers\Admin\BrandController;
 use App\Http\Controllers\Admin\CommentController;
-use App\Http\Controllers\Admin\AuthController;
-use App\Http\Controllers\Admin\OrderController;
-use App\Http\Controllers\Admin\ImageController;
-use App\Http\Controllers\Admin\ProductController;
-use App\Http\Controllers\Admin\CartController;
-use App\Http\Controllers\Client\MailController;
-use App\Http\Controllers\Admin\UserController;
-use App\Http\Controllers\Client\ProductController as ClientProductController;
-use App\Http\Controllers\Admin\AuthController;
 
 Route::prefix('admin')->group(function () {
-
     Route::apiResource('brands', BrandController::class);
     Route::apiResource('blog', BlogController::class);
     Route::apiResource('blogcategory', BlogCategoryController::class);
-    Route::apiResource('user', UserController::class);
-Route::apiResource('productCategory', CategoryController::class);
+    Route::apiResource('productCategory', CategoryController::class);
     Route::apiResource('role', RoleController::class);
     Route::apiResource('comment', CommentController::class);
-    Route::apiResource('productCategory', CategoryController::class);
-
-
-    Route::apiResource('categories', CategoryController::class);
-    Route::apiResource('products', ProductController::class);
-    Route::get('/search', [ProductController::class, 'search']);//http://localhost:8000/api/client/search?query=teneanpham
-    Route::apiResource('image', ImageController::class);
-    Route::apiResource('orders', OrderController::class);
-    Route::apiResource('cart', CartController::class);;
-});
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/register', [AuthController::class, 'Register']);
-
-Route::prefix('client')->group(function () {
-    Route::get('/products/search', [ClientProductController::class, 'search']);//http://localhost:8000/api/client/products/search?query=teneanpham
-    Route::get('send-mail', [ClientProductController::class, 'sendMail']);//http://localhost:8000/api/client/products/search?query=teneanpham
-    Route::post('/contact/send', [MailController::class, 'send']);
-    Route::middleware('auth:api')->post('/checkout', [CheckoutController::class, 'checkout']);
-
 });
