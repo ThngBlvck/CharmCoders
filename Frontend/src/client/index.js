1b3f24e6e6f5eae9445e88cac995627943bbb903
JackKy17471 <148345405+JackKy17471@users.noreply.github.com>
2024-09-22 19:42:35 +0700

    thêm giao diện cart,checkout,forgotpw,login,oderM,post,postdetail,profile,regis

--- Frontend/src/client/index.js
+++ Frontend/src/client/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Routes, Route} from "react-router-dom";
+import {Routes, Route, Navigate} from "react-router-dom";
 import {CartProvider} from './components/Cart';
 import Footer from "./components/Footer";
 import Home from "./Pages/Home";
@@ -8,6 +8,15 @@ import Contact from "./Pages/Contact";
 import Products from "./Pages/Products";
 import ProductDetail from "./Pages/ProductDetail";
 import Header from "./components/Header";
+import Profile from "./Pages/profile";
+import Cart from "./Pages/Cart";
+import Checkout from "./Pages/Checkout";
+import Register from "./Pages/Register";
+import Login from "./Pages/Login";
+import ForgotPassword from "./Pages/Forgot_PW";
+import Ordered from "./Pages/OrderManagement";
+import Post from "./Pages/Post";
+import PostDetail from "./Pages/PostDetail";
 
 
 export default function Client() {
@@ -22,7 +31,17 @@ export default function Client() {
                     <Route path="/contact" element={<Contact/>}/>
                     <Route path="/products" element={<Products/>}/>
                     <Route path="/products/:id" element={<ProductDetail/>}/>
+                    <Route path="/profile" element={<Profile/>}/>
+                    <Route path="/cart" element={<Cart/>}/>
+                    <Route path="/checkout" element={<Checkout/>} />
+                    <Route path="/register" element={<Register/>} />
+                    <Route path="/login" element={<Login/>} />
+                    <Route path="/forgot-password" element={<ForgotPassword/>} />
+                    <Route path="/ordered" element={<Ordered/>} />
+                    <Route path="/post" element={<Post/>} />
+                    <Route path="/postdetail" element={<PostDetail/>} />
 
+                    <Route path="/" element={<Navigate to="/home" />} />
                 </Routes>
             </div>
             <Footer/>
