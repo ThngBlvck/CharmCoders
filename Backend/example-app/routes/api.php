dfd6e852ea8292ea80d26f3b9afbae8c7702beb2
tranchinguyen1307 <145844563+tranchinguyen1307@users.noreply.github.com>
2024-09-26 12:16:40 +0700

    Sửa lại route

--- Backend/example-app/routes/api.php
+++ Backend/example-app/routes/api.php
@@ -5,18 +5,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\BlogCategoryController;
 use App\Http\Controllers\Admin\BlogController;
-use App\Http\Controllers\Admin\ProductController;
 use App\Http\Controllers\Admin\RoleController;
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
-
-
-
 use App\Http\Controllers\Admin\BrandController;
 use App\Http\Controllers\Admin\CommentController;
-use App\Http\Controllers\Admin\AuthController;
 use App\Http\Controllers\Admin\OrderController;
 use App\Http\Controllers\Admin\ImageController;
 use App\Http\Controllers\Admin\ProductController;
@@ -24,9 +15,9 @@ use App\Http\Controllers\Admin\CartController;
 use App\Http\Controllers\Client\MailController;
 use App\Http\Controllers\Admin\UserController;
 use App\Http\Controllers\Client\ProductController as ClientProductController;
+use App\Http\Controllers\Admin\AuthController;
 
 Route::prefix('admin')->group(function () {
-
     Route::apiResource('brands', BrandController::class);
     Route::apiResource('blog', BlogController::class);
     Route::apiResource('blogcategory', BlogCategoryController::class);
@@ -45,6 +36,11 @@ Route::prefix('admin')->group(function () {
     Route::apiResource('image', ImageController::class);
     Route::apiResource('orders', OrderController::class);
     Route::apiResource('cart', CartController::class);
+
+    Route::apiResource('categories', CategoryController::class);
+    Route::apiResource('products', ProductController::class);
+    Route::apiResource('image', ImageController::class);
+    Route::apiResource('orders', OrderController::class);
 });
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'Register']);
