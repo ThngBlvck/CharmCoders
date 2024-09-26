e41606da9f6395fb532ad886f3df62218605a51d
tranchinguyen1307 <145844563+tranchinguyen1307@users.noreply.github.com>
2024-09-26 12:16:40 +0700

    Sửa lại route

--- Backend/example-app/routes/api.php
+++ Backend/example-app/routes/api.php
@@ -5,15 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\BlogCategoryController;
 use App\Http\Controllers\Admin\BlogController;
-use App\Http\Controllers\Admin\ProductController;
 use App\Http\Controllers\Admin\RoleController;
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
-
-
-
 use App\Http\Controllers\Admin\BrandController;
 use App\Http\Controllers\Admin\CommentController;
 use App\Http\Controllers\Admin\AuthController;
@@ -26,11 +18,14 @@ use App\Http\Controllers\Admin\UserController;
 use App\Http\Controllers\Client\ProductController as ClientProductController;
 
 Route::prefix('admin')->group(function () {
-
     Route::apiResource('brands', BrandController::class);
     Route::apiResource('blog', BlogController::class);
     Route::apiResource('blogcategory', BlogCategoryController::class);
-    Route::apiResource('productCategory', CategoryController::class);
+    Route::apiResource('user', UserController::class);
+Route::apiResource('productCategory', CategoryController::class);
+    Route::apiResource('role', RoleController::class);
+    Route::apiResource('comment', CommentController::class);
+Route::apiResource('productCategory', CategoryController::class);
     Route::apiResource('role', RoleController::class);
     Route::apiResource('comment', CommentController::class);
 
@@ -52,6 +47,7 @@ Route::prefix('client')->group(function () {
     Route::post('/contact/send', [MailController::class, 'send']);
     Route::middleware('auth:api')->post('/checkout', [CheckoutController::class, 'checkout']);
 
+});
 
 });
 Route::post('/login', [AuthController::class, 'login']);
