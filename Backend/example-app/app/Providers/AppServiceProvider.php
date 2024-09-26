02be28362c8ba1a01e1461990d4ff5159d1c3b3b
toankhung <lyminhtoan2004@gmail.com>
2024-09-26 16:30:48 +0700

    login user/admin

--- Backend/example-app/app/Providers/AppServiceProvider.php
+++ Backend/example-app/app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Laravel\Passport\Passport;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Passport::loadKeysFrom(__DIR__.'/../secrets/oauth');
+        Passport::tokensCan([
+            'admin' => 'Quản lý hệ thống',
+            'user' => 'Chỉ truy cập chức năng người dùng',
+        ]);
+        Passport::setDefaultScope([
+            'user',
+        ]);
     }
 }
--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -4,7 +4,7 @@ import Client from "./client";
 import {Routes, Route} from 'react-router-dom';
 import './App.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
-
+import PrivateRoute from './PrivateRoute';
 import "./assets/styles/tailwind.css";
 import "./assets/styles/css/style.css";
 import "./assets/styles/css/bootstrap.min.css";
@@ -12,9 +12,14 @@ import "./assets/styles/css/bootstrap.min.css";
 
 
 const App = () => {
+
+    const isAuthenticated = !!localStorage.getItem("token");
+    const userRole = localStorage.getItem("role"); 
     return (
         <Routes>
-            <Route path="/admin/*" element={<Admin/>}></Route>
+              <Route element={<PrivateRoute isAuthenticated={isAuthenticated} userRole={userRole} />}>
+                <Route path="/admin/*" element={<Admin />} />
+            </Route>
             <Route path="/*" element={<Client/>}></Route>
         </Routes>
     )
