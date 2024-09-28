033c695781fb032cdddb23a3935b57118ddf595c
tranchinguyen1307 <145844563+tranchinguyen1307@users.noreply.github.com>
2024-09-28 11:52:18 +0700

    Gửi mail liên hệ (Client)

--- Backend/example-app/routes/api.php
+++ Backend/example-app/routes/api.php
@@ -8,12 +8,34 @@ use App\Http\Controllers\Admin\BlogController;
 use App\Http\Controllers\Admin\RoleController;
 use App\Http\Controllers\Admin\BrandController;
 use App\Http\Controllers\Admin\CommentController;
+use App\Http\Controllers\Admin\OrderController;
+use App\Http\Controllers\Admin\ImageController;
+use App\Http\Controllers\Admin\ProductController;
+use App\Http\Controllers\Admin\CartController;
+use App\Http\Controllers\Client\MailController;
+use App\Http\Controllers\Admin\UserController;
+use App\Http\Controllers\Client\ProductController as ClientProductController;
 
 Route::prefix('admin')->group(function () {
     Route::apiResource('brands', BrandController::class);
     Route::apiResource('blog', BlogController::class);
     Route::apiResource('blogcategory', BlogCategoryController::class);
-    Route::apiResource('productCategory', CategoryController::class);
-    Route::apiResource('role', RoleController::class);
-    Route::apiResource('comment', CommentController::class);
+    Route::apiResource('user', UserController::class);
+
+    Route::apiResource('roles', RoleController::class);
+    Route::apiResource('comments', CommentController::class);
+
+    Route::apiResource('categories', CategoryController::class);
+    Route::apiResource('products', ProductController::class);
+    Route::get('/search', [ProductController::class, 'search']);//http://localhost:8000/api/client/search?query=teneanpham
+    Route::apiResource('image', ImageController::class);
+    Route::apiResource('orders', OrderController::class);
+    Route::apiResource('cart', CartController::class);
+});
+
+Route::prefix('client')->group(function () {
+    Route::get('/products/search', [ClientProductController::class, 'search']);//http://localhost:8000/api/client/products/search?query=teneanpham
+    Route::get('send-mail', [ClientProductController::class, 'sendMail']);//http://localhost:8000/api/client/products/search?query=teneanpham
+    Route::post('/contact', [MailController::class, 'sendMail']);//mail
+
 });
