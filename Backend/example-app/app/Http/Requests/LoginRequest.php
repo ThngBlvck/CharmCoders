0c4ba8a0a0be011a4dc6c067e284e5bbc91a39fe
toankhung <lyminhtoan2004@gmail.com>
2024-10-02 12:24:57 +0700

    Auth Controller

---
+++ Backend/example-app/app/Http/Requests/LoginRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class LoginRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return true; // Để cho phép mọi người sử dụng request này
+    }
+
+    public function rules()
+    {
+        return [
+            'email' => 'required|email',
+            'password' => 'required|min:5',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'email.required' => 'Email là bắt buộc.',
+            'email.email' => 'Email không hợp lệ.',
+            'password.required' => 'Mật khẩu là bắt buộc.',
+            'password.min' => 'Mật khẩu phải có ít nhất 5 ký tự.',
+        ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'success' => false,
+            'message' => $validator->errors(),
+        ], 422));
+    }
+}
