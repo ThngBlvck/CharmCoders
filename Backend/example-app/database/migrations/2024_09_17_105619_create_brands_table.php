314c6fc3abcf744220b757c30e23b5ce3a846ac2
tranchinguyen1307 <145844563+tranchinguyen1307@users.noreply.github.com>
2024-10-02 14:16:11 +0700

    thêm cột status

--- Backend/example-app/database/migrations/2024_09_17_105619_create_brands_table.php
+++ Backend/example-app/database/migrations/2024_09_17_105619_create_brands_table.php
@@ -16,6 +16,7 @@ class CreateBrandsTable extends Migration
         Schema::create('brands', function (Blueprint $table) {
             $table->id(); // Tạo trường id với kiểu INT và auto-increment
             $table->string('name')->nullable(); // Trường name kiểu VARCHAR
+            $table->tinyInteger('status')->default(1);
             $table->text('image')->nullable(); // Trường image kiểu TEXT
             $table->timestamps(); // Tạo các trường created_at và updated_at
         });
