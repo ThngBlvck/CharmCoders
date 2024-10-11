ce0369b15622e739de57347c0572ede0948e3958
toankhung <lyminhtoan2004@gmail.com>
2024-10-11 13:14:04 +0700

    logout fontend

--- Backend/example-app/routes/api.php
+++ Backend/example-app/routes/api.php
@@ -10,18 +10,19 @@ use App\Http\Controllers\Admin\BrandController;
 use App\Http\Controllers\Admin\CommentController;
 use App\Http\Controllers\Admin\AuthController;
 use App\Http\Controllers\Admin\OrderController;
+use App\Http\Controllers\Client\OrderController as OrderClient;
 use App\Http\Controllers\Admin\ImageController;
 use App\Http\Controllers\Admin\CartController;
 
 use App\Http\Controllers\Admin\ProductController;
 use App\Http\Controllers\Client\MailController;
+use App\Http\Controllers\SocialiteController;
 use App\Http\Controllers\Admin\UserController;
 use App\Http\Controllers\Client\ProductController as ClientProductController;
 use App\Http\Controllers\Auth\ResetPasswordController;
 use App\Http\Controllers\Client\CheckoutController;
 use App\Http\Controllers\Client\CartController as CartClient;
-use App\Http\Controllers\Client\MoMoController;
-use App\Http\Controllers\Client\OrderController as OrderClient;
+
 
 Route::prefix('admin')->group(function () {
 
@@ -38,52 +39,41 @@ Route::prefix('admin')->group(function () {
     Route::apiResource('products', ProductController::class);
     Route::get('/search', [ProductController::class, 'search']); //http://localhost:8000/api/client/search?query=teneanpham
     Route::apiResource('image', ImageController::class);
-    Route::apiResource('orders', OrderController::class);
     Route::middleware('auth:api')->group(function () {
+        Route::apiResource('orders', OrderController::class);
         Route::apiResource('cart', CartController::class);
     });
 
     Route::apiResource('orders', OrderController::class);
     Route::apiResource('employee', UserController::class);
-    Route::post('/comments', [CommentController::class, 'store']); // New route to add a comment
-
-    // New route to get comments by product ID
 });
-
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'Register']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:api');
-
+Route::get('/auth/redirect/{provider}', [SocialiteController::class, 'authProviderRedirect']);
+Route::get('/auth/callback/{provider}', [SocialiteController::class, 'socialAuthentication']);
 
 Route::prefix('client')->group(function () {
     Route::middleware('auth:api')->group(function () {
-        Route::post('/checkout-buy-now', [CheckoutController::class, 'checkout']);
-        Route::get('/getAllCart', [CartClient::class, 'getCart']);
-        Route::post('/buy-now', [CheckoutController::class, 'buyNow']);
-        Route::post('/checkout-all', [CheckoutController::class, 'checkoutCart']);
         Route::apiResource('orders', OrderClient::class);
-Route::get('comments/product/{productId}', [CommentController::class, 'getCommentsByProductId']);
-
-
-
+        Route::post('/checkout', [CheckoutController::class, 'checkout']);
+        Route::get('/getAllCart', [CartClient::class, 'getCart']);
+        Route::post('/select-cart', [CheckoutController::class, 'showSelectedCartsByIds']);
+        Route::post('/buy-now', [CheckoutController::class, 'buyNow']);
+        Route::get('/user', [UserController::class, 'getUser'])->middleware('auth:api');
     });
+    Route::middleware('auth:api')->post('/checkout', [CheckoutController::class, 'checkout']); //http://localhost:8000/api/client/products/search?query=teneanpham
+    Route::middleware('auth:api')->get('/getCart', [CartClient::class, 'getCart']); //http://localhost:8000/api/client/products/search?query=teneanpham
     Route::get('/products/search', [ClientProductController::class, 'search']); //http://localhost:8000/api/client/products/search?query=teneanpham
-    Route::get('/send-mail', [ClientProductController::class, 'sendMail']); //http://localhost:8000/api/client/products/search?query=teneanpham
+    Route::get('send-mail', [ClientProductController::class, 'sendMail']); //http://localhost:8000/api/client/products/search?query=teneanpham
     Route::post('/contact/send', [MailController::class, 'send']);
     // Route để yêu cầu đặt lại mật khẩu qua API
     Route::post('password/reset', [ResetPasswordController::class, 'reset'])->name('client.reset-password');
     Route::middleware('auth:api')->get('/user', [UserController::class, 'getUser']);
-    Route::middleware('auth:api')->apiResource('comments', CommentController::class);
 });
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'Register']);
-Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:api');
-
 
 Route::post('password/send-otp', [ResetPasswordController::class, 'sendOtp']);
 Route::post('password/verify-otp', [ResetPasswordController::class, 'verifyOtp']);
 Route::post('password/reset', [ResetPasswordController::class, 'resetPassword']);
-
-
-
-
