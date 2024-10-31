ed96a4cdd064e53da2704949f8dbeb15d290e747
JackKy17471 <148345405+JackKy17471@users.noreply.github.com>
2024-10-31 11:30:36 +0700

    reset Password

--- Backend/example-app/routes/api.php
+++ Backend/example-app/routes/api.php
@@ -38,8 +38,8 @@ Route::prefix('admin')->group(function () {
     Route::apiResource('products', ProductController::class);
     Route::get('/search', [ProductController::class, 'search']); //http://localhost:8000/api/client/search?query=teneanpham
     Route::apiResource('image', ImageController::class);
+    Route::apiResource('orders', OrderController::class);
     Route::middleware('auth:api')->group(function () {
-        Route::apiResource('orders', OrderController::class);
         Route::apiResource('cart', CartController::class);
     });
 
@@ -81,3 +81,7 @@ Route::middleware('auth:api')->apiResource('comments', CommentController::class)
 Route::post('password/send-otp', [ResetPasswordController::class, 'sendOtp']);
 Route::post('password/verify-otp', [ResetPasswordController::class, 'verifyOtp']);
 Route::post('password/reset', [ResetPasswordController::class, 'resetPassword']);
+
+
+
+
