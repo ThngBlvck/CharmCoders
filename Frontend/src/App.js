44c8ae933143ac185875466c8262b4086b6f09da
ThngBlvck <thonghhpc05659@fpt.edu.vn>
2024-10-31 12:03:46 +0700

    Bắt lỗi keyword route 404

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,28 +1,26 @@
 import React from "react";
 import Admin from './admin';
 import Client from "./client";
-import {Routes, Route} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import PrivateRoute from './PrivateRoute';
 import "./assets/styles/tailwind.css";
 import "./assets/styles/css/style.css";
 import "./assets/styles/css/bootstrap.min.css";
-
-
+import { UserProvider } from "./contexts/UserContext";
 
 const App = () => {
-
-    const isAuthenticated = !!localStorage.getItem("token");
-    const userRole = localStorage.getItem("role"); 
     return (
-        <Routes>
-              <Route element={<PrivateRoute isAuthenticated={isAuthenticated} userRole={userRole} />}>
-                <Route path="/admin/*" element={<Admin />} />
-            </Route>
-            <Route path="/*" element={<Client/>}></Route>
-        </Routes>
-    )
-}
+        <UserProvider>
+            <Routes>
+                <Route element={<PrivateRoute />}>
+                    <Route path="/admin/*" element={<Admin />} />
+                </Route>
+                <Route path="/*" element={<Client />} />
+            </Routes>
+        </UserProvider>
+    );
+};
 
 export default App;
--- Frontend/src/admin/index.js
+++ Frontend/src/admin/index.js
@@ -9,55 +9,43 @@ import Footer from "./components/Footer";
 
 // views
 import Dashboard from "./Pages/Dashboard";
+import Page404 from "../client/Pages/404";
 
-//Product
+// Product
 import Product from "./Pages/Product/List";
-import ProductDetail from "./Pages//Product/ProductDetail";
+import ProductDetail from "./Pages/Product/ProductDetail";
 import AddProduct from "./Pages/Product/Add";
 import EditProduct from "./Pages/Product/Edit";
 
-//CategoryProduct
+// CategoryProduct
 import ProductCategory from "./Pages/ProductCategory/List";
 import AddProductCategory from "./Pages/ProductCategory/Add";
 import EditProductCaterogy from "./Pages/ProductCategory/Edit";
 
-//User
+// User
 import UserList from "./Pages/User/List";
 import AddEmployee from "./Pages/User/Add";
 import EditEmployee from "./Pages/User/Edit";
 
+// Blog and BlogCategory
 import Blog from "./Pages/Blog/List";
-import Brand from "./Pages/Brand/List";
-import Comment from "./Pages/Comment/List";
-import Role from "./Pages/Role/List";
-import Order from "./Pages/Order";
-
 import BlogCaterogy from "./Pages/BlogCategory/List";
-
-
-
-
-
-
 import AddBlogCategory from "./Pages/BlogCategory/Add";
 import EditBlogCategory from "./Pages/BlogCategory/Edit";
-import EditProductCaterogy from "./Pages/ProductCategory/Edit";
-
-import EditBrand from "./Pages/Brand/Edit";
-import AddBrand from "./Pages/Brand/Add";
-
-
-import EditBrand from "./Pages/Brand/Edit";
-import AddBrand from "./Pages/Brand/Add";
-
-import AddRole from "./Pages/Role/Add";
-import EditRole from "./Pages/Role/Edit";
-
 import AddBlog from "./Pages/Blog/Add";
 import EditBlog from "./Pages/Blog/Edit";
 
-// import Settings from "views/admin/Settings.js";
-// import Tables from "views/admin/Tables.js";
+// Brand
+import Brand from "./Pages/Brand/List";
+import AddBrand from "./Pages/Brand/Add";
+import EditBrand from "./Pages/Brand/Edit";
+
+// Other sections
+import Comment from "./Pages/Comment/List";
+import Role from "./Pages/Role/List";
+import Order from "./Pages/Order";
+import AddRole from "./Pages/Role/Add";
+import EditRole from "./Pages/Role/Edit";
 
 export default function Admin() {
     return (
@@ -71,41 +59,48 @@ export default function Admin() {
                     <Routes>
                         <Route path="dashboard" element={<Dashboard />} />
 
+                        {/* Product Category Routes */}
                         <Route path="category_product" element={<ProductCategory />} />
                         <Route path="category_product/add" element={<AddProductCategory />} />
                         <Route path="category_product/edit/:id" element={<EditProductCaterogy />} />
 
+                        {/* Product Routes */}
                         <Route path="product" element={<Product />} />
                         <Route path="product/add" element={<AddProduct />} />
                         <Route path="product/edit/:id" element={<EditProduct />} />
                         <Route path="product/detail/:id" element={<ProductDetail />} />
 
+                        {/* User Routes */}
                         <Route path="user" element={<UserList />} />
                         <Route path="user/add" element={<AddEmployee />} />
                         <Route path="user/edit/:id" element={<EditEmployee />} />
 
+                        {/* Blog Category Routes */}
                         <Route path="category_blog" element={<BlogCaterogy />} />
                         <Route path="category_blog/add" element={<AddBlogCategory />} />
                         <Route path="category_blog/edit/:id" element={<EditBlogCategory />} />
 
-                        <Route path="blog" element={<Blog />}/>
-                        <Route path="blog/add" element={<AddBlog />}/>
-                        <Route path="blog/edit/:id" element={<EditBlog />}/>
+                        {/* Blog Routes */}
+                        <Route path="blog" element={<Blog />} />
+                        <Route path="blog/add" element={<AddBlog />} />
+                        <Route path="blog/edit/:id" element={<EditBlog />} />
 
+                        {/* Brand Routes */}
+                        <Route path="brand" element={<Brand />} />
+                        <Route path="brand/add" element={<AddBrand />} />
+                        <Route path="brand/edit/:id" element={<EditBrand />} />
 
+                        {/* Other Routes */}
+                        <Route path="comment" element={<Comment />} />
+                        <Route path="order" element={<Order />} />
+                        <Route path="role" element={<Role />} />
+                        <Route path="role/add" element={<AddRole />} />
+                        <Route path="role/edit/:id" element={<EditRole />} />
 
-                        <Route path="brand" element={<Brand />}/>
-                        <Route path="brand/add" element={<AddBrand />}/>
-                        <Route path="brand/edit/:id" element={<EditBrand />}/>
-
-
-
-                        <Route path="comment" element={<Comment />}/>
-                        <Route path="order" element={<Order />}/>
-                        <Route path="role" element={<Role />}/>
-                        <Route path="role/add" element={<AddRole />}/>
-                        <Route path="role/edit/:id" element={<EditRole />}/>
+                        {/* Default and 404 Route */}
                         <Route path="/" element={<Navigate to="/admin/dashboard" />} />
+                        <Route path="/404" element={<Page404 />} />
+                        <Route path="*" element={<Navigate to="/404" />} />
                     </Routes>
                     <Footer />
                 </div>
