3f82b8ecfde42cf1b8d40b56f93867088a3c0995
JackKy17471 <148345405+JackKy17471@users.noreply.github.com>
2024-11-09 15:44:15 +0700

    fix loi vnpay

--- Backend/example-app/config/vnpay.php
+++ Backend/example-app/config/vnpay.php
@@ -3,5 +3,5 @@ return [
     'vnp_tmn_code' => env('VNP_TMN_CODE', 'your_vnp_tmn_code'),
     'vnp_hash_secret' => env('VNP_HASH_SECRET', 'your_secret_key'),
     'vnp_url' => env('VNP_URL', 'https://sandbox.vnpayment.vn/paymentv2/vpcpay.html'),
-    'vnp_return_url' => env('VNP_RETURN_URL', 'http://localhost:8000/vnpay-return'), // link này sẽ xử lý sau khi thanh toán thành công
+    'vnp_return_url' => env('VNP_RETURN_URL', 'http://localhost:8000/api/client/payment-return'), // link này sẽ xử lý sau khi thanh toán thành công
 ];
