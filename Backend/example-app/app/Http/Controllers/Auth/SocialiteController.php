78857056cd92335f38dc5b7cf0d1a9f3c656ff69
toankhung <lyminhtoan2004@gmail.com>
2024-11-09 15:44:33 +0700

    change password

--- Backend/example-app/app/Http/Controllers/Auth/SocialiteController.php
+++ Backend/example-app/app/Http/Controllers/Auth/SocialiteController.php
@@ -32,12 +32,12 @@ class SocialiteController extends Controller
         try {
             if ($provider) {
                 // Sử dụng stateless() để đảm bảo không sử dụng session
-                $socialUser = Socialite::driver($provider)->stateless()->with(['prompt' => 'select_account']) ->user();
+                $socialUser = Socialite::driver($provider)->stateless()->with(['prompt' => 'select_account'])->user();
                 \Log::info('Social User Data: ', (array) $socialUser);
-
+    
                 // Đặt giá trị mặc định cho role (nếu cần)
                 $role = 1; // Mặc định là người dùng
-
+    
                 // Sử dụng firstOrCreate để tìm hoặc tạo người dùng
                 $user = User::firstOrCreate(
                     ['auth_provider_id' => $socialUser->id],
@@ -48,15 +48,21 @@ class SocialiteController extends Controller
                         'role_id' => $role, // Thiết lập giá trị role mặc định
                     ]
                 );
-
+    
+                // Xóa tất cả các token cũ của người dùng (nếu có)
+                $user->tokens->each(function ($token) {
+                    $token->delete();
+                });
+    
                 // Đăng nhập người dùng
                 Auth::login($user);
-
+    
                 // Tạo token Passport cho người dùng
-                $token = $user->createToken('Personal Access Token')->accessToken;
-
+                $token = $user->createToken('Socical Access Token')->accessToken;
+    
                 return response()->json(['token' => $token, 'user' => $user], 200);
             }
+    
             return response()->json(['error' => 'Provider not found.'], 404);
         } catch (\Exception $e) {
             // Ghi log lỗi ra file với thông tin chi tiết
@@ -64,6 +70,7 @@ class SocialiteController extends Controller
             return response()->json(['error' => 'Lỗi xảy ra: ' . $e->getMessage()], 500);
         }
     }
+    
 
 
 
