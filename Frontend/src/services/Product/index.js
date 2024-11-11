c8aaf10d00a50a1fd4096e9d23d65e2436633453
JackKy17471 <148345405+JackKy17471@users.noreply.github.com>
2024-11-11 17:38:21 +0700

    - Sản phẩm liên quan

--- Frontend/src/services/Product/index.js
+++ Frontend/src/services/Product/index.js
@@ -1,12 +1,11 @@
 import request from '../../api';
 
 const URL_Product = 'admin/products';
-
 const URL_Search = 'admin/search';  // Thay đổi URL nếu cần
-
 const URL_Checkout = 'client/buy-now';
 const URL_MomoPayment = 'momo-payment';
-
+const URL_related = 'client/related-products';
+// Hàm tìm kiếm sản phẩm
 export const searchProduct = (query) => {
     return request({
         method: 'GET',
@@ -14,6 +13,7 @@ export const searchProduct = (query) => {
     });
 };
 
+// Hàm lấy danh sách sản phẩm
 export const getProduct = (query = '') => {
     return request({
         method: 'GET',
@@ -24,7 +24,7 @@ export const getProduct = (query = '') => {
     });
 };
 
-// Các hàm CRUD khác không thay đổi
+// Hàm lấy thông tin 1 sản phẩm
 export const getOneProduct = (id) => {
     return request({
         method: 'GET',
@@ -32,6 +32,7 @@ export const getOneProduct = (id) => {
     });
 };
 
+// Hàm lấy danh sách sản phẩm theo ID
 export const getProductsByIds = (ids) => {
     return request({
         method: 'GET',
@@ -39,7 +40,7 @@ export const getProductsByIds = (ids) => {
     });
 };
 
-
+// Hàm thêm mới sản phẩm
 export const postProduct = (data) => {
     return request({
         method: 'POST',
@@ -48,6 +49,7 @@ export const postProduct = (data) => {
     });
 };
 
+// Hàm cập nhật sản phẩm
 export const updateProduct = (id, data) => {
     return request({
         method: 'POST',
@@ -56,6 +58,7 @@ export const updateProduct = (id, data) => {
     });
 };
 
+// Hàm xóa sản phẩm
 export const deleteProduct = (id) => {
     return request({
         method: 'DELETE',
@@ -84,5 +87,13 @@ export const makeMomoPayment = (amount, orderId, orderInfo) => {
             orderId,      // ID của đơn hàng (có thể tự sinh hoặc từ backend)
             orderInfo     // Thông tin đơn hàng để hiển thị khi thanh toán
         }
+    });
+};
+
+// Hàm lấy sản phẩm liên quan
+export const getRelatedProducts = (id) => {
+    return request({
+        method: 'GET',
+        path: `client/products/related/${id}`, // Đường dẫn tương ứng với API route
     });
 };
