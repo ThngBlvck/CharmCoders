f1276a899190d982cfd1f1d670f3a0a5db35a952
tranchinguyen1307 <145844563+tranchinguyen1307@users.noreply.github.com>
2024-11-11 11:24:49 +0700

    sửa thanh toán vnpay

--- Backend/example-app/config/vnpay.php
+++ Backend/example-app/config/vnpay.php
@@ -1,7 +1,7 @@
 <?php
 return [
-    'vnp_tmn_code' => env('VNP_TMN_CODE', 'your_vnp_tmn_code'),
-    'vnp_hash_secret' => env('VNP_HASH_SECRET', 'your_secret_key'),
+    'vnp_tmn_code' => env('VNP_TMN_CODE', 'L1517K15'),
+    'vnp_hash_secret' => env('VNP_HASH_SECRET', 'LNQZYBKFL029QSB1OYGH3GP4LGH6ADWY'),
     'vnp_url' => env('VNP_URL', 'https://sandbox.vnpayment.vn/paymentv2/vpcpay.html'),
     'vnp_return_url' => env('VNP_RETURN_URL', 'http://localhost:8000/api/client/payment-return'), // link này sẽ xử lý sau khi thanh toán thành công
 ];
