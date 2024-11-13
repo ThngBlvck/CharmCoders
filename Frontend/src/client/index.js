ce7b574c90642f2af8835ae201dd533e04a765a8
ThngBlvck <thonghhpc05659@fpt.edu.vn>
2024-11-13 20:52:50 +0700

    fix nhẹ

--- Frontend/src/client/index.js
+++ Frontend/src/client/index.js
@@ -24,9 +24,8 @@ import VerifyOtp from "./Pages/Otp_PW";
 import Edit_Profile from "./Pages/Edit_Profile";
 import List_Address from "./Pages/Address/List_Address";
 import Add_Address from "./Pages/Address/Add_Address";
-import Edit_Address from "./Pages/Edit_Profile";
-import ChangePassword from "./Pages/Change_Password";
 import Edit_Address from "./Pages/Address/Edit_Address";
+import ChangePassword from "./Pages/Change_Password";
 
 import GoogleCallback from "./Pages/GoogleCallback";
 import OrderDetail from "./Pages/Order_Detail";
