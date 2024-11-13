b75a351fb7b804e33101c5f935921245bf75f901
HoaiTam30102003 <hoaitam30102003@gmail.com>
2024-11-13 20:28:26 +0700

    Fix giao diện cart

--- Frontend/src/services/Order/index.js
+++ Frontend/src/services/Order/index.js
@@ -3,8 +3,6 @@ import request from '../../api';
 const URL_Order = 'client/orders';
 const URL_Update_Order = 'admin/orders';
 const URL_Order_Admin = 'admin/orders';
-const URL_Update_Order = 'admin/orders';
-
 
 export const getOrder = (query = '') => {
     return request({
@@ -42,15 +40,5 @@ export const getOrderAdmin = (query = '') => {
     return request({
         method: 'GET',
         path: `${URL_Order_Admin}`,
-    });
-};
-
-export const updateOrder = (id, status) => {
-    return request({
-        method: 'POST',
-        path: `${URL_Update_Order}/${id}?_method=PUT`,
-        data: {
-            status: status, // Truyền status là chuỗi thay vì đối tượng
-        },
     });
 };
