ea52155d9a54155da2259233267256d5914ec7b2
ThngBlvck <thonghhpc05659@fpt.edu.vn>
2024-12-16 01:22:00 +0700

    Fix Lọc đơn hàng và chi tiết đơn hàng

--- Frontend/src/admin/components/Footer/index.js
+++ Frontend/src/admin/components/Footer/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Footer() {
     return (
@@ -11,46 +12,38 @@ export default function Footer() {
                             <div className="text-sm text-blueGray-500 font-semibold py-1 text-center md:text-left">
                                 Copyright © {new Date().getFullYear()}{" "}
                                 <a
-                                    href="https://www.creative-tim.com?ref=nr-footer-admin"
+                                    href="#"
                                     className="text-blueGray-500 hover:text-blueGray-700 text-sm font-semibold py-1"
                                 >
-                                    Creative Tim
+                                    Glow Maker
                                 </a>
                             </div>
                         </div>
                         <div className="w-full md:w-8/12 px-4">
-                            <ul className="flex flex-wrap list-none md:justify-end  justify-center">
+                            <ul className="flex flex-wrap list-none md:justify-end justify-center">
                                 <li>
-                                    <a
-                                        href="https://www.creative-tim.com?ref=nr-footer-admin"
+                                    <Link
+                                        to="/home"
                                         className="text-blueGray-600 hover:text-blueGray-800 text-sm font-semibold block py-1 px-3"
                                     >
-                                        Creative Tim
-                                    </a>
+                                        Glow Maker
+                                    </Link>
                                 </li>
                                 <li>
-                                    <a
-                                        href="https://www.creative-tim.com/presentation?ref=nr-footer-admin"
+                                    <Link
+                                        to="/about"
                                         className="text-blueGray-600 hover:text-blueGray-800 text-sm font-semibold block py-1 px-3"
                                     >
-                                        About Us
-                                    </a>
+                                        Về Chúng Tôi
+                                    </Link>
                                 </li>
                                 <li>
-                                    <a
-                                        href="http://blog.creative-tim.com?ref=nr-footer-admin"
+                                    <Link
+                                        to="/post"
                                         className="text-blueGray-600 hover:text-blueGray-800 text-sm font-semibold block py-1 px-3"
                                     >
-                                        Blog
-                                    </a>
-                                </li>
-                                <li>
-                                    <a
-                                        href="https://github.com/creativetimofficial/notus-react/blob/main/LICENSE.md?ref=nr-footer-admin"
-                                        className="text-blueGray-600 hover:text-blueGray-800 text-sm font-semibold block py-1 px-3"
-                                    >
-                                        MIT License
-                                    </a>
+                                        Bài Viết
+                                    </Link>
                                 </li>
                             </ul>
                         </div>
