723957d15d66a54e9836873e4d9095a51bc61ed5
tranchinguyen1307 <145844563+tranchinguyen1307@users.noreply.github.com>
2024-12-17 14:46:38 +0700

    bắt lỗi giá sale phải lớn hớn giá gốc

--- Backend/example-app/app/Http/Requests/Admin/StoreProductRequest.php
+++ Backend/example-app/app/Http/Requests/Admin/StoreProductRequest.php
@@ -27,7 +27,7 @@ class StoreProductRequest extends FormRequest
         $rules = [
             'name' => 'required|string|max:255|unique:products,name',
             'unit_price' => 'required|numeric|min:0', // Giá gốc phải là số dương hoặc bằng 0
-            'sale_price' => 'nullable|numeric|min:0', // Giá giảm phải là số dương hoặc bằng 0
+            'sale_price' => 'nullable|numeric|min:0|lte:unit_price', // Giá giảm phải là số dương hoặc bằng 0
             'quantity' => 'required|integer|min:0', // Số lượng phải là số nguyên và lớn hơn hoặc bằng 0
             'sku' => 'nullable|string|unique:products,sku', // SKU là chuỗi và duy nhất
             'content' => 'nullable|string',
@@ -35,8 +35,6 @@ class StoreProductRequest extends FormRequest
             'status' => 'required|boolean',
             'brand_id' => 'nullable|exists:brands,id',
             'category_id' => 'nullable|exists:categories,id',
-            // Kiểm tra nếu có biến thể
-            'has_variants' => 'nullable|boolean',
         ];
 
         // Nếu là phương thức PUT hoặc PATCH (chỉnh sửa sản phẩm)
@@ -58,12 +56,6 @@ class StoreProductRequest extends FormRequest
         }
 
         // Kiểm tra nếu có biến thể trong request, nếu có thì yêu cầu các thuộc tính biến thể
-        if ($this->has('variants')) {
-            $rules['variants.*.attributes'] = 'required|array'; // Kiểm tra thuộc tính của biến thể
-            $rules['variants.*.attributes.*'] = 'required|exists:attributes,id'; // Kiểm tra sự tồn tại của thuộc tính
-            $rules['variants.*.price'] = 'nullable|numeric|min:0'; // Giá của biến thể
-            $rules['variants.*.quantity'] = 'nullable|integer|min:0'; // Số lượng của biến thể
-        }
 
         return $rules;
     }
@@ -89,8 +81,6 @@ class StoreProductRequest extends FormRequest
             'quantity.required' => 'Số lượng sản phẩm là bắt buộc.',
             'quantity.integer' => 'Số lượng sản phẩm phải là số nguyên.',
             'quantity.min' => 'Số lượng sản phẩm phải lớn hơn hoặc bằng 0.',
-            'sku.string' => 'SKU phải là chuỗi ký tự.',
-            'sku.unique' => 'SKU này đã tồn tại.',
             'content.string' => 'Nội dung phải là chuỗi ký tự.',
             'views.integer' => 'Lượt xem phải là số nguyên.',
             'views.min' => 'Lượt xem phải lớn hơn hoặc bằng 0.',
@@ -98,6 +88,7 @@ class StoreProductRequest extends FormRequest
             'status.boolean' => 'Trạng thái sản phẩm phải là giá trị đúng hoặc sai.',
             'brand_id.exists' => 'Thương hiệu không tồn tại.',
             'category_id.exists' => 'Danh mục không tồn tại.',
+            'sale_price.lte' => 'Giá giảm không được lớn hơn giá gốc.',
         ];
     }
 
